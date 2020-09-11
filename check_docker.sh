dd49340270be9d55601f2cd85863e85633e4dd6c
Kanga-Who <jason.reibelt@gmail.com>
2020-09-11 19:58:10 +1000

    Update check_docker.sh

--- check_docker.sh
+++ check_docker.sh
@@ -2,7 +2,7 @@
 	if [ -x docker -e /var/run/docker.sock] then
             echo "Docker-CE is already installed"
             
-        else
+        elif
             echo "Docker-ce is not present - installing now"  
             curl -fsSL get.docker.com | sh
     fi
