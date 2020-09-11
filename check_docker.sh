924edc6712f3387b1e6a3b1ae3394f804776bf32
Kanga-Who <jason.reibelt@gmail.com>
2020-09-11 23:02:03 +1000

    Update check_docker.sh

--- check_docker.sh
+++ check_docker.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-    if command -v docker > /dev/null 2>&1 then
-        echo "Docker-ce is not present - installing now"  
+    if command -v docker > /dev/null 2>&1; then
+        echo "Docker is installed "
+      else
+        echo "Docker is not present - installing now"
         curl -fsSL get.docker.com | sh
     fi
