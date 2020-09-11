bfaf16946d44782ef585079b1de5f6aaf0f982bc
Kanga-Who <jason.reibelt@gmail.com>
2020-09-11 14:06:45 +1000

    Create required.sh

---
+++ required.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+function info { echo -e "[Info] $*"; }
+function error { echo -e "[Error] $*"; exit 1; }
+function warn  { echo -e "[Warning] $*"; }
+
+test $? -eq 0 || exit 1 "you should have sudo priveledge to run this script"
+
+info "Installing the must-have pre-requisites"
+
+while read -r p ; do sudo apt-get install -y $p ; done < <(cat << "EOF"
+	software-properties-common
+	apparmor-utils 
+	apt-transport-https 
+	avahi-daemon 
+	ca-certificates 
+	curl 
+	dbus 
+	jq 
+	network-manager
+EOF
+)
+
+fi
