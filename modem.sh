73cef6aba3c263775f4ee230fe36c67e3ad8ddc0
Kanga-Who <jason.reibelt@gmail.com>
2020-09-11 17:09:44 +1000

    Delete modem.sh

--- modem.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-systemctl status ModemManager | grep 'active (running)' > /dev/null 2>&1
-
-if [ $? != 0 ]
-then
-        systemctl disable ModemManager > /dev/null
-	systemctl stop ModemManager > /dev/null
-fi
