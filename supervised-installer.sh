c0d43dc6eff574b3a4b4cf1717717cdaee77fa6f
Kanga-Who <jason.reibelt@gmail.com>
2020-10-20 08:02:35 +1000

    Update supervised-installer.sh
    Update from main script.

--- supervised-installer.sh
+++ supervised-installer.sh
@@ -29,36 +29,38 @@ SERVICE_DOCKER="docker.service"
 SERVICE_NM="NetworkManager.service"
 
 FILE_DOCKER_CONF="/etc/docker/daemon.json"
+FILE_INTERFACES="/etc/network/interfaces"
 FILE_NM_CONF="/etc/NetworkManager/NetworkManager.conf"
 FILE_NM_CONNECTION="/etc/NetworkManager/system-connections/default"
 
 URL_RAW_BASE="https://raw.githubusercontent.com/home-assistant/supervised-installer/master/files"
 URL_VERSION="https://version.home-assistant.io/stable.json"
+URL_BIN_APPARMOR="${URL_RAW_BASE}/hassio-apparmor"
+URL_BIN_HASSIO="${URL_RAW_BASE}/hassio-supervisor"
 URL_DOCKER_DAEMON="${URL_RAW_BASE}/docker_daemon.json"
+URL_HA="${URL_RAW_BASE}/ha"
+URL_INTERFACES="${URL_RAW_BASE}/interfaces"
 URL_NM_CONF="${URL_RAW_BASE}/NetworkManager.conf"
 URL_NM_CONNECTION="${URL_RAW_BASE}/system-connection-default"
-URL_HA="${URL_RAW_BASE}/ha"
-URL_BIN_HASSIO="${URL_RAW_BASE}/hassio-supervisor"
-URL_BIN_APPARMOR="${URL_RAW_BASE}/hassio-apparmor"
-URL_SERVICE_HASSIO="${URL_RAW_BASE}/hassio-supervisor.service"
 URL_SERVICE_APPARMOR="${URL_RAW_BASE}/hassio-apparmor.service"
+URL_SERVICE_HASSIO="${URL_RAW_BASE}/hassio-supervisor.service"
 URL_APPARMOR_PROFILE="https://version.home-assistant.io/apparmor.txt"
 
 # Check env
-command -v systemctl > /dev/null 2>&1 || MISSING_PACAKGES+=("systemd")
-command -v nmcli > /dev/null 2>&1 || MISSING_PACAKGES+=("NetworkManager")
-command -v apparmor_parser > /dev/null 2>&1 || MISSING_PACAKGES+=("AppArmor")
-command -v docker > /dev/null 2>&1 || MISSING_PACAKGES+=("docker")
-command -v jq > /dev/null 2>&1 || MISSING_PACAKGES+=("jq")
-command -v curl > /dev/null 2>&1 || MISSING_PACAKGES+=("curl")
-command -v avahi-daemon > /dev/null 2>&1 || MISSING_PACAKGES+=("avahi")
-command -v dbus-daemon > /dev/null 2>&1 || MISSING_PACAKGES+=("dbus")
+command -v systemctl > /dev/null 2>&1 || MISSING_PACKAGES+=("systemd")
+command -v nmcli > /dev/null 2>&1 || MISSING_PACKAGES+=("NetworkManager")
+command -v apparmor_parser > /dev/null 2>&1 || MISSING_PACKAGES+=("AppArmor")
+command -v docker > /dev/null 2>&1 || MISSING_PACKAGES+=("docker")
+command -v jq > /dev/null 2>&1 || MISSING_PACKAGES+=("jq")
+command -v curl > /dev/null 2>&1 || MISSING_PACKAGES+=("curl")
+command -v avahi-daemon > /dev/null 2>&1 || MISSING_PACKAGES+=("avahi")
+command -v dbus-daemon > /dev/null 2>&1 || MISSING_PACKAGES+=("dbus")
 
 
-if [ ! -z "${MISSING_PACAKGES}" ]; then
-    warn "The folowing is missing on the host and needs "
+if [ ! -z "${MISSING_PACKAGES}" ]; then
+    warn "The following is missing on the host and needs "
     warn "to be installed and configured before running this script again"
-    error "missing: ${MISSING_PACAKGES[@]}"
+    error "missing: ${MISSING_PACKAGES[@]}"
 fi
 
 # Check if Modem Manager is enabled
@@ -95,11 +97,22 @@ fi
 
 # Create config for NetworkManager
 info "Creating NetworkManager configuration"
-rm -f /etc/network/interfaces
 curl -sL "${URL_NM_CONF}" > "${FILE_NM_CONF}"
 if [ ! -f "$FILE_NM_CONNECTION" ]; then
     curl -sL "${URL_NM_CONNECTION}" > "${FILE_NM_CONNECTION}"
 fi
+
+warn "Changes are needed to the /etc/network/interfaces file"
+info "If you have modified the network on the host manualy, those can now be overwritten"
+info "If you do not overwrite this now you need to manually adjust it later"
+info "Do you want to proceed with that? [N/y] "
+read answer
+
+if [[ "$answer" =~ "y" ]] || [[ "$answer" =~ "Y" ]]; then
+    info "Replacing /etc/network/interfaces"
+    curl -sL "${URL_INTERFACES}" > "${FILE_INTERFACES}";
+fi
+
 info "Restarting NetworkManager"
 systemctl restart "${SERVICE_NM}"
 
