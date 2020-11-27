cfb4240d0434cc2cf18b090d15be5d8bfd226532
Kanga-Who <jason.reibelt@gmail.com>
2020-11-28 09:24:42 +1000

    Delete test.sh

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if systemctl list-unit-files ModemManager.service | grep enabled; then
-
-	sudo systemctl disable ModemManager > /dev/null
-        sudo systemctl stop ModemManager > /dev/null
-fi
