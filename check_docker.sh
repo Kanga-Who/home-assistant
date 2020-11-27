a3088a4340ed0c0d80a664116ea84fa944577fb9
Kanga-Who <jason.reibelt@gmail.com>
2020-11-28 09:24:33 +1000

    Delete check_docker.sh

--- check_docker.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-    if command -v docker > /dev/null 2>&1; then
-        echo "Docker is installed "
-      else
-        echo "Docker is not present - installing now"
-        curl -fsSL get.docker.com | sh
-    fi
