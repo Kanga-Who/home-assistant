e5abc2b05e5f3d5f990ae97e863ca2d2783babee
Kanga-Who <jason.reibelt@gmail.com>
2021-01-02 11:25:42 +1000

    Update hassos_install.sh

--- hassos_install.sh
+++ hassos_install.sh
@@ -137,7 +137,7 @@ pvesm alloc $STORAGE $VMID $DISK0 128 1>&/dev/null
 qm importdisk $VMID ${FILE%".xz"} $STORAGE ${IMPORT_OPT:-} 1>&/dev/null
 qm set $VMID \
   -efidisk0 ${DISK0_REF},size=128K \
-  -sata0 ${DISK1_REF},size=24G > /dev/null
+  -sata0 ${DISK1_REF},size=18G > /dev/null
 qm set $VMID \
   -boot order=sata0 > /dev/null
 
